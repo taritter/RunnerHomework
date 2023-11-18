b2f576d6822b027040fd6b7f29b10243ab9e0777
Therese Ritter <98991018+taritter@users.noreply.github.com>
2023-11-18 10:55:31 -0800

    Add files via upload

---
+++ rect.cpp
@@ -0,0 +1,175 @@
+#include "graphics.h"
+#include "rect.h"
+#include <iostream>
+using namespace std;
+
+/********************* Dimensions Struct ********************/
+
+dimensions::dimensions() : width(0), height(0) {}
+
+dimensions::dimensions(double w, double h) : width(w), height(h) {}
+
+ostream& operator << (ostream& outs, const dimensions &d) {
+    outs << "[" << d.width << ", " << d.height << "]";
+    return outs;
+}
+
+
+Rect::Rect() : Shape(), size({0, 0}) {
+}
+
+Rect::Rect(dimensions size) : Shape() {
+    setSize(size);
+}
+
+Rect::Rect(color fill) : Shape(fill), size({0, 0}) {
+}
+
+Rect::Rect(point2D center) : Shape(center), size({0, 0}) {
+}
+
+Rect::Rect(color fill, point2D center) : Shape(fill, center), size({0, 0}) {
+}
+
+Rect::Rect(double red, double green, double blue, double alpha) : Shape(red, green, blue, alpha), size({0, 0}) {
+}
+
+Rect::Rect(double x, double y) : Shape(x, y), size({0, 0}) {
+}
+
+Rect::Rect(double red, double green, double blue, double alpha, double x, double y) : Shape(red, green, blue, alpha, x, y), size({0, 0}) {
+}
+
+Rect::Rect(color fill, double x, double y) : Shape(fill, x, y), size({0, 0}) {
+}
+
+Rect::Rect(double red, double green, double blue, double alpha, point2D center) : Shape(red, green, blue, alpha, center), size({0, 0}) {
+}
+
+Rect::Rect(color fill, dimensions size) : Shape(fill) {
+    setSize(size);
+}
+
+Rect::Rect(point2D center, dimensions size) : Shape(center) {
+    setSize(size);
+}
+
+Rect::Rect(color fill, point2D center, dimensions size) : Shape(fill, center) {
+    setSize(size);
+}
+
+Rect::Rect(double red, double green, double blue, double alpha, dimensions size) : Shape(red, green, blue, alpha) {
+    setSize(size);
+}
+
+Rect::Rect(double x, double y, dimensions size) : Shape(x, y) {
+    setSize(size);
+}
+
+Rect::Rect(double red, double green, double blue, double alpha, double x, double y, dimensions size) : Shape(red, green, blue, alpha, x, y) {
+    setSize(size);
+}
+
+Rect::Rect(color fill, double x, double y, dimensions size) : Shape(fill, x, y) {
+    setSize(size);
+}
+
+Rect::Rect(double red, double green, double blue, double alpha, point2D center, dimensions size) : Shape(red, green, blue, alpha, center) {
+    setSize(size);
+}
+
+dimensions Rect::getSize() const {
+    return size;
+}
+
+double Rect::getWidth() const {
+    return size.width;
+}
+
+double Rect::getHeight() const {
+    return size.height;
+}
+
+double Rect::getLeftX() const {
+    return center.x - (size.width / 2.0);
+}
+
+double Rect::getRightX() const {
+    return center.x + (size.width / 2.0);
+}
+
+double Rect::getTopY() const {
+    return center.y - (size.height / 2.0);
+}
+
+double Rect::getBottomY() const {
+    return center.y + (size.height / 2.0);
+}
+
+void Rect::setSize(dimensions size) {
+    if (size.width >= 0 && size.height >= 0) {
+        this->size = size;
+    }
+}
+
+void Rect::setSize(double width, double height) {
+    setSize({width, height});
+}
+
+void Rect::setWidth(double width) {
+    setSize({width, size.height});
+}
+
+void Rect::setHeight(double height) {
+    setSize({size.width, height});
+}
+
+void Rect::changeSize(double deltaWidth, double deltaHeight) {
+    setSize({size.width + deltaWidth, size.height + deltaHeight});
+}
+
+void Rect::changeWidth(double delta) {
+    setSize({size.width + delta, size.height});
+}
+
+void Rect::changeHeight(double delta) {
+    setSize({size.width, size.height + delta});
+}
+
+// TODO: Implement this method
+bool Rect::isOverlapping(const Rect &r) const {
+    // There are only two cases when rectangles are *not* overlapping:
+    // 1. when one is to the left of the other
+    // 2. when one is above the other
+    if(this->getCenterX() == r.getCenterX() && this->getCenterY() == r.getCenterY()){
+        return true;
+    }
+    else{
+        if(this->getRightX() < r.getLeftX()) {
+            return false;
+        }
+        else if(r.getRightX() < this->getLeftX()) {
+            return false;
+        }
+        else if(this->getTopY() > r.getBottomY()) {
+            return false;
+        }
+        else if(r.getTopY() > this->getBottomY()) {
+            return false;
+        }
+        return true;
+    }
+}
+
+// TODO: Implement this method
+void Rect::draw() const {
+    glBegin(GL_QUADS);
+    // Don't forget to set the color to the fill field
+    color c = getColor();
+    glColor3f(c.red, c.green, c. blue);
+    glVertex2i(getLeftX(),getBottomY());
+    glVertex2i(getLeftX(),getTopY());
+    glVertex2i(getRightX(),getTopY());
+    glVertex2i(getRightX(),getBottomY());
+    glEnd();
+}
